6d6597e46579e0b81f5176718806e4732842108a
Hakan KOCAK <hakankocak@Hakans-MBP.mynet>
2020-04-11 18:24:04 +0300

    Redux boilerplate setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+//Redux
+import { Provider } from 'react-redux'
+import store from './store'
 
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+        </Fragment>
+      </Router>
+    </Provider>
+  )
+}
 export default App;
