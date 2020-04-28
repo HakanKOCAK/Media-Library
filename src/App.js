ed2663fd19bda9a3f67f9018aa8a219a794d8397
Hakan KOCAK <hakankocak@Hakans-MBP.mynet>
2020-04-28 05:20:40 +0300

    404 page created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Auth from './auth/Auth';
 import Navbar from './components/Navbar'
 import Landing from './components/Landing'
@@ -7,6 +7,7 @@ import Login from './components/Login'
 import Register from './components/Register'
 import Files from './components/files/Files'
 import FileDetails from './components/files/FileDetails'
+import NoMatch from './components/NoMatch'
 
 //Redux
 import { Provider } from 'react-redux'
@@ -23,12 +24,14 @@ const App = () => {
       <Auth>
         <Router>
           <Navbar />
-          <Route exact path="/" component={Landing} />
           <Switch >
+            <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/files" component={Files} />
             <Route path="/files/:id" component={FileDetails} />
+            <Route exact path="/error-404" component={NoMatch} />
+            <Redirect from="*" to='/error-404' />
           </Switch>
         </Router>
       </Auth>
