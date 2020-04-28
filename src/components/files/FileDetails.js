f641c224ff54cfb0fde1b43e30d1219cd67a2904
Hakan KOCAK <hakankocak@Hakans-MBP.mynet>
2020-04-28 10:32:56 +0300

    Extract selected file from redux store

--- src/components/files/FileDetails.js
+++ src/components/files/FileDetails.js
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import Spinner from '../spinner/Spinner';
 
 const FileDetails = (props) => {
+    const { id } = props.match.params
+
+    const files = useSelector(({ files }) => files.entities)
+
+    const [file, setFile] = useState(null);
+
+    useEffect(() => {
+        setFile(files[id])
+    }, [files])
+
+    if (!file) {
+        return <Spinner />
+    }
     return (
         <div>
             file details
