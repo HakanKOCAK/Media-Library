fb91e9b5a65a70b1b45c2c079592311ec100596b
Hakan KOCAK <hakankocak@Hakans-MBP.mynet>
2020-05-06 11:53:36 +0300

    Error messages moved to /Utils/errors

---
+++ src/Utils/errors.js
@@ -0,0 +1,8 @@
+export const errorMessages = {
+    PASSWORDS_DO_NOT_MATCH: 'Passwords do not match.',
+    PASSWORD_LENGTH: 'Minimum password lenght is 6.',
+    EMAIL_FORMAT: 'Wrong email format.',
+    NO_ACCOUNT: 'User Not Found. Please check your credentials.',
+    USER_DISABLED: 'Sorry, it seems like the account has been disabled by an administrator.',
+    USER_EXIST: 'User already exist.'
+}
--- src/components/Error.js
+++ src/components/Error.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { errorMessages } from '../Utils/errors';
 
 const Error = props => {
-
-    const errorMessages = {
-        PASSWORDS_DO_NOT_MATCH: 'Passwords do not match.',
-        PASSWORD_LENGTH: 'Minimum password lenght is 6.',
-        EMAIL_FORMAT: 'Wrong email format.',
-        NO_ACCOUNT: 'User Not Found. Please check your credentials.',
-        USER_DISABLED: 'Sorry, it seems like the account has been disabled by an administrator.',
-        USER_EXIST: 'User already exist.'
-    }
-
     const { flags } = props;
 
     console.log(flags)
