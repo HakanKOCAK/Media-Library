1aeda1263223a01722b6fd995e93fa46d4363e0b
Hakan KOCAK <hakankocak@Hakans-MBP.mynet>
2020-05-14 10:57:51 +0300

    Link to JotForm to create new submission

--- src/components/files/FileList.js
+++ src/components/files/FileList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../../styles/Files.css'
@@ -24,48 +24,58 @@ const FileList = (props) => {
         props.history.push('/files/' + item.submissionId);
     }
     return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Name Surname</th>
-                    <th>E-mail</th>
-                    <th>Upload Date</th>
-                    <th>File Name</th>
-                    <th>File Type</th>
-                    <th>File Size</th>
-                    <th>Duration</th>
-                </tr>
-            </thead>
-            <tbody>
-                {filesArray.map((item) => {
-                    return (
-                        <tr key={item.submissionId} onClick={event => handleClick(event, item)}>
-                            <td>
-                                {item.nameSurname}
+        <Fragment>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Name Surname</th>
+                        <th>E-mail</th>
+                        <th>Upload Date</th>
+                        <th>File Name</th>
+                        <th>File Type</th>
+                        <th>File Size</th>
+                        <th>Duration</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filesArray.map((item) => {
+                        return (
+                            <tr key={item.submissionId} onClick={event => handleClick(event, item)}>
+                                <td>
+                                    {item.nameSurname}
+                                </td>
+                                <td>
+                                    {item.email}
+                                </td>
+                                <td>
+                                    {item.uploadDate}
+                                </td>
+                                <td>
+                                    {item.entity.fileName}
+                                </td>
+                                <td>
+                                    {item.fileType}
+                                </td>
+                                <td>
+                                    {item.entity.size}
+                                </td>
+                                <td>
+                                    -
                             </td>
-                            <td>
-                                {item.email}
-                            </td>
-                            <td>
-                                {item.uploadDate}
-                            </td>
-                            <td>
-                                {item.entity.fileName}
-                            </td>
-                            <td>
-                                {item.fileType}
-                            </td>
-                            <td>
-                                {item.entity.size}
-                            </td>
-                            <td>
-                                -
-                            </td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://form.jotform.com/201324174735046"
+                className="newLink"
+            >
+                New File
+            </a>
+        </Fragment>
     )
 }
 
