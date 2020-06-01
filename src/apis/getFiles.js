733d7f04e38427c0174a3153ac24d26aa82f441c
Hakan KOCAK <hakankocak@Hakans-MBP.mynet>
2020-06-01 11:08:37 +0300

    Code refactors

--- src/apis/getFiles.js
+++ src/apis/getFiles.js
@@ -7,7 +7,6 @@ const normalizeResponse = (question, submissionDetails, key) => {
   const { name: questionName, answer: questionAnswer } = question;
   const fileType = [];
   fileType.push(newSubmissionDetails.fileType);
-  console.log('filetype', fileType);
   switch (questionName) {
     case 'nameSurname':
       newSubmissionDetails = {
@@ -113,16 +112,17 @@ const getAnswers = async (data) => data.reduce((answers, submissionDetails) => {
   const submission = submissionDetails.answers;
   const createdAt = submissionDetails.created_at.split(' ')[0].split('-');
   const date = `${createdAt[2]}/${createdAt[1]}/${createdAt[0]}`;
-  const reduceSubmissionDetails = Object.entries(submission).reduce((newSubmissionDetails, value) => {
-    const key = value[0];
-    const question = value[1];
-    const newData = normalizeResponse(question, { ...newSubmissionDetails }, key);
-    return {
-      submissionId: submissionDetails.id,
-      uploadDate: date,
-      ...newData,
-    };
-  }, {});
+  const reduceSubmissionDetails = Object.entries(submission)
+    .reduce((newSubmissionDetails, value) => {
+      const key = value[0];
+      const question = value[1];
+      const newData = normalizeResponse(question, { ...newSubmissionDetails }, key);
+      return {
+        submissionId: submissionDetails.id,
+        uploadDate: date,
+        ...newData,
+      };
+    }, {});
   newAnswers[submissionDetails.id] = reduceSubmissionDetails;
   return newAnswers;
 }, {});
