867227cc959a17971be02e845a1430b426c98ba4
Hakan KOCAK <hakankocak@Hakans-MBP.mynet>
2020-06-02 15:21:17 +0300

    Auth component design updates/on progress check

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Spinner from './spinner/Spinner';
 
 const Auth = (props) => {
   const { onConfirmDelete, onCloseDialog } = props;
   const [password, setPassword] = useState('');
+  const [onProgress, setProgress] = useState(false);
+
+  const config = {
+    PASSWORD_LENGTH: false,
+  };
+
+  const [flags, setFlags] = useState(config);
+
+  useEffect(() => {
+    if (password && password.length < 6) {
+      setFlags({ ...flags, PASSWORD_LENGTH: true });
+    } else {
+      setFlags({ ...flags, PASSWORD_LENGTH: false });
+    }
+  }, [password]);
 
   const onConfirm = () => {
+    setProgress(true);
     onConfirmDelete(password);
   };
 
@@ -19,14 +36,21 @@ const Auth = (props) => {
       <input
         type="password"
         name="password"
+        className={flags.PASSWORD_LENGTH ? 'error' : ''}
         placeholder="Password"
         onChange={(event) => { event.preventDefault(); setPassword(event.target.value); }}
         value={password}
       />
-      <div className="button-div">
-        <button type="button" className="btn btn-light" onClick={onClose}>Cancel</button>
-        <button type="button" className="btn btn-primary" onClick={onConfirm}>Ok</button>
-      </div>
+      {
+        !onProgress
+          ? (
+            <div className="button-div">
+              <button type="button" className="btn btn-light" onClick={onClose}>Cancel</button>
+              <button disabled={flags.PASSWORD_LENGTH} type="button" className="btn btn-primary" onClick={onConfirm}>Ok</button>
+            </div>
+          )
+          : <Spinner modified />
+      }
     </>
   );
 };
