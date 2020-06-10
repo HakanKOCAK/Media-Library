ba21ca06a2aa6ae9b12c0d69efdd0eb5abd70fbf
Hakan KOCAK <hakankocak@Hakans-MBP.mynet>
2020-06-10 17:31:31 +0300

    Email regex check update

--- src/Utils/regExp.js
+++ src/Utils/regExp.js
@@ -1,4 +1,4 @@
 export const formatCheckToFromForm = (input) => /^((?:[01]\d|2[0-3])-[0-5]\d-[0-5]\d)\/((?:[01]\d|2[0-3])-[0-5]\d-[0-5]\d)$/.test(input);
 export const formatCheckFromInput = (input) => /^((?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d)$/.test(input);
 export const nameSurnameCheck = (input) => /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšşğıžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u.test(input);
-export const emailCheck = (input) => /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(input);
+export const emailCheck = (input) => /^[a-zA-Z0-9-_.]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(input);
--- src/config/config.js
+++
@@ -1,2 +0,0 @@
-export const apiKey = '{your api key}';
-export const formId = '{your form id}';
