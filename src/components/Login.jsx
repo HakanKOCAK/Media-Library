5b1c20dde7e6219fcef3eac49e2a04f3fa20ddbc
Hakan KOCAK <hakankocak@Hakans-MBP.mynet>
2020-06-11 15:36:27 +0300

    Password marked as required in Login Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -119,6 +119,7 @@ const Login = () => {
             placeholder="Password"
             name="userPassword"
             minLength="6"
+            required
             value={password}
             onChange={(event) => onChange(event)}
           />
